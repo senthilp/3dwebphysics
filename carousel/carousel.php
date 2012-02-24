30aca32f4f0a86f2bd4b40129842fb10887cb391
Senthil Padmanabhan <senthil.padmanabhan@yahoo.com>
2012-02-24 15:09:39 -0800

    First cut

---
+++ carousel/carousel.php
@@ -0,0 +1,28 @@
+<?php
+?>
+<!DOCTYPE html>
+<html>
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+	<title>Garage 360 - A 3D Experience</title>
+	<link type="text/css" href="carousel.css" rel="stylesheet"/>
+    <!--[if IE]>
+    <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+    <![endif]-->	
+</head>
+<body>
+	<header>
+	
+	</header>
+	<div class="content">
+	
+	</div>
+	<footer>
+	
+	</footer>	
+	<script src="carousel.js"></script>		
+	<script>
+
+	</script>
+</body>
+</html>
